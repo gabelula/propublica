2da60ed5bb0ed4807f509f3cafed260d077110bc
Gabriela Rodriguez <gabelula@gmail.com>
2017-07-05 19:30:47 -0700

    Using pandas is a better clearer approach.

--- merge.py
+++ merge.py
@@ -3,7 +3,7 @@
 
 # Script that creates a CSV that has all the information from the Open Payments provider table, plus each provider's NPI.
 
-import csv
+import pandas as pd
 import optparse
 from yaml import load, dump
 
@@ -17,43 +17,11 @@ from yaml import load, dump
 def get_mapping_fields():
     with open('map.yml', 'rb') as csv_file:
         data = load(csv_file)
-
         return data
 
-# Look for the NPI field by fields and its values. 
-def search_npi(file, npi_field, fields):
-    
-    with open(file, 'rb') as csv_file:
-        npis = csv.DictReader(csv_file)
-        for row in npis:
-            temp = filter(lambda x: row[x] == fields[x], fields)
-            if temp and reduce(lambda x, y: x and y, temp):
-                return row[npi_field]
-    
-    print 'NPI not found for fields {0}'.format(fields)
-    return None 
-
-
-def get_values_to_identify_physician(row):
-
-    fields = {}    
-    map = get_mapping_fields()
-
-    for k in map['fields']:
-        fields[map['fields'][k]] = row[k]
-
-    return fields
-
-
-def process_rows(payments, nppes_file):
-    rows = []
-    for row in payments:
-        fields = get_values_to_identify_physician(row)
-        row['NPI'] = search_npi(nppes_file, 'NPI', fields)
-        rows.append(row)
-    return rows
-
 def main():
+    
+    # Get the options from the command line.
     p = optparse.OptionParser()
     p.add_option('--payments', '-p', default='etl_data/01-openpayments_physicians.csv')
     p.add_option('--out', '-o', default='etl_data/openpayments_physicians_with_npi.csv')
@@ -65,18 +33,32 @@ def main():
     new_payments_file = options.out
     nppes_file = options.nppes
 
-    with open(payments_file, 'rb') as csv_file:
-        payments = csv.DictReader(csv_file)
-        
-        new_fieldnames = payments.fieldnames
-        new_fieldnames.append('NPI')
 
-        with open(new_payments_file, 'wb') as new_csv_file:
-            payments_with_npi = csv.DictWriter(new_csv_file, fieldnames=new_fieldnames)
-            payments_with_npi.writeheader()
-            new_rows = process_rows(payments, nppes_file)
-            payments_with_npi.writerows(new_rows)
-            
+    # The fields to do the join between files.
+    fields_to_map = get_mapping_fields()
+
+    # The files to merge.
+    payments = pd.read_csv(payments_file)
+    nppes = pd.read_csv(nppes_file)#, usecols=fields_to_map.keys)
+
+    # Because pandas merge use sensitive case we need to lower the case for both.
+    for key in fields_to_map:
+        nppes[key] = nppes[key].str.lower()
+        payments[fields_to_map[key]] = payments[fields_to_map[key]].str.lower()
+    
+    # Confirm that our fields to map are indexes of the CSV tables.
+
+    # Left Join on the payments file with the file that has the NPI by the physician name.
+    full_merged = pd.merge(payments, nppes, how='left', left_on=fields_to_map.values(), right_on=fields_to_map.keys(), indicator=True)
+    
+    # Only get the columns that I want to save back.
+    new_columns = list(payments.columns.values)
+    new_columns.append('NPI')
+
+    merged = full_merged.ix[:, new_columns]
+
+    # Save to the new CSV file.
+    merged.to_csv(new_payments_file, index=False)
 
 if __name__ == '__main__':
     main()
